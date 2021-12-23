c726352549407226565c4e7e0a158094cff447e6
CritianChipana <cristianchipanahuaman@gmail.com>
2021-12-23 17:26:08 -0500

    se agrego cuadro estadistico

--- views/CuadroEstadistico/js.js
+++ views/CuadroEstadistico/js.js
@@ -1,98 +1,59 @@
-const config = {
-  type: 'line',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart'
-      }
-    }
-  },
-};
-const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+let ctx = document.getElementById('myChart').getContext('2d');
 
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-    },
-    {
-      label: 'Dataset 2',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.blue,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-    }
-  ]
-};
+let meta = [90,89,88,87,86,85];
+let desplazo= [90,90,89];
 
-const actions = [
+let dataset2 = [
   {
-    name: 'Randomize',
-    handler(chart) {
-      chart.data.datasets.forEach(dataset => {
-        dataset.data = Utils.numbers({count: chart.data.labels.length, min: -100, max: 100});
-      });
-      chart.update();
+    label: 'META',
+    data: meta,
+    backgroundColor: [
+      'rgb(54, 162, 235)',
+    ],
+    borderColor:[
+    'rgb(54, 162, 235)',
+    ],
+    parsing: {  
+    yAxisKey: 'net'
     }
   },
   {
-    name: 'Add Dataset',
-    handler(chart) {
-      const data = chart.data;
-      const dsColor = Utils.namedColor(chart.data.datasets.length);
-      const newDataset = {
-        label: 'Dataset ' + (data.datasets.length + 1),
-        backgroundColor: Utils.transparentize(dsColor, 0.5),
-        borderColor: dsColor,
-        data: Utils.numbers({count: data.labels.length, min: -100, max: 100}),
-      };
-      chart.data.datasets.push(newDataset);
-      chart.update();
+    label: 'YO',
+    data: desplazo,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+    ],
+    borderColor: [
+    'rgb(255, 99, 132)',
+    ],            
+    parsing: {
+        yAxisKey: 'cogs'
     }
   },
-  {
-    name: 'Add Data',
-    handler(chart) {
-      const data = chart.data;
-      if (data.datasets.length > 0) {
-        data.labels = Utils.months({count: data.labels.length + 1});
-
-        for (var index = 0; index < data.datasets.length; ++index) {
-          data.datasets[index].data.push(Utils.rand(-100, 100));
-        }
-
-        chart.update();
-      }
-    }
-  },
-  {
-    name: 'Remove Dataset',
-    handler(chart) {
-      chart.data.datasets.pop();
-      chart.update();
-    }
-  },
-  {
-    name: 'Remove Data',
-    handler(chart) {
-      chart.data.labels.splice(-1, 1); // remove the label first
-
-      chart.data.datasets.forEach(dataset => {
-        dataset.data.pop();
-      });
-
-      chart.update();
-    }
-  }
+  // {
+  //   label: 'Tercer Arreglo',
+  //   data: [50,40,30,20,10,0],
+  //   backgroundColor: [
+  //     'rgb(255, 205, 86)'
+  //   ],
+  //   borderColor: [
+  //   'rgb(255, 205, 86)'
+  //   ],
+  //   parsing: {
+  //       yAxisKey: 'cogs'
+  //   }
+  // }
 ];
+
+
+let data2 ={
+
+  labels: ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Dicienbre"],
+  datasets: dataset2
+};
+
+
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: data2
+});
